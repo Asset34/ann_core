99c0c5a39e32777b3ee25ebd15947ed48bfac443
Asset34 <artemiy.kashintsev@gmail.com>
2018-10-07 02:39:30 +0300

    ActivateFunction: implement abstract class

---
+++ src/activationfunction.hpp
@@ -0,0 +1,11 @@
+#ifndef ACTIVATIONFUNCTION_HPP
+#define ACTIVATIONFUNCTION_HPP
+
+class ActivationFunction
+{
+public:
+    double operator()(double x) = 0;
+
+};
+
+#endif // ACTIVATIONFUNCTION_HPP
