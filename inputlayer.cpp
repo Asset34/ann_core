7e0796d642a6e86c5d27c1351fe0408faed0a5e9
Asset34 <artemiy.kashintsev@gmail.com>
2018-11-24 17:22:07 +0300

    InputLayer: implement move semantics

--- inputlayer.cpp
+++ inputlayer.cpp
@@ -1,6 +1,8 @@
 #include "inputlayer.hpp"
 #include "inputneuron.hpp"
 
+#include <utility>
+
 InputLayer::InputLayer(size_t n)
 {
     m_neurons.resize(n);
@@ -17,6 +19,16 @@ InputLayer::InputLayer(const WeightVec &vec)
     for (size_t i = 0; i < vec.size(); i++) {
         m_neurons[i] = new InputNeuron(vec[i]);
     }
+}
+
+InputLayer::InputLayer(InputLayer &&other)
+    : Layer(std::move(other))
+{
+}
+
+InputLayer &InputLayer::operator=(InputLayer &&other)
+{
+    Layer::operator=(std::move(other));
 }
 
 void InputLayer::setInputs(const std::vector<double> &vec)
--- inputlayer.hpp
+++ inputlayer.hpp
@@ -9,6 +9,11 @@ class InputLayer : public Layer
 public:
     explicit InputLayer(size_t n);
     explicit InputLayer(const WeightVec &vec);
+    InputLayer(const InputLayer &other) = delete;
+    InputLayer(InputLayer &&other);
+
+    InputLayer &operator=(const InputLayer &other) = delete;
+    InputLayer &operator=(InputLayer &&other);
 
     void setInputs(const std::vector<double> &vec);
 
